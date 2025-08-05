0d8c9232cf26c1d880bd6be17d653fb7bca08016
Jeffrey Busold <jeffemail@fake.com>
2025-08-05 16:07:58 -0400

    navigation with react router implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import './App.css';
 import ProductList from './components/ProductList';
 import Login from './components/Login';
 import Register from './components/Register';
+import Home from './components/Home';
+import { Route, Routes, Navigate, NavLink} from 'react-router';
 
 function App() {
   return (
     <div id='main' >
       <h1>ExitTraining Storefront</h1>
-      <ProductList />
-      <Login />
-      <Register />
+      <nav>
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/products">Products</NavLink>
+        <NavLink to="/register">Register</NavLink>
+        <NavLink to="/login">Login</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<ProductList />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (<div className="app">
+        <h2>Home Page</h2>
+            <div>
+                Welcome to ExitCertified Storefront, a place where you can buy miscellaneous products at affordable prices! Use the bar above to navigate to the different pages of the store.
+            </div>
+    </div>);
+};
+
+export default Home;
