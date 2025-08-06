3ab7376de0756f28173e83b51424e8d7499f4f6e
Jeffrey Busold <jeffemail@fake.com>
2025-08-06 11:27:07 -0400

    functional search bar + image progress

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
     return (<div className="app">
         <h2>Home Page</h2>
             <div>
-                Welcome to ExitCertified Storefront, a place where you can buy miscellaneous products at affordable prices! Use the bar above to navigate to the different pages of the store.
+                <p>Welcome to ExitCertified Storefront, a place where you can buy miscellaneous products at affordable prices! Use the bar above to navigate to the different pages of the store.</p>
             </div>
     </div>);
 };
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -10,22 +10,32 @@ const ProductList = () => {
         });
     };
     useEffect(() => {getProducts()}, [] );
+    const searchFilter = (event) => {
+        event.preventDefault();
+        const search = document.getElementById("search").value;
+        console.log(search);
+        const result = products.filter(function (prod){
+           return prod.name === search;
+        });
+        console.log(result);
+        setProducts(result);
+    };
     //console.log(products);
     return (<div className='app'>
         <h2>Product List</h2>
         <form className="searchForm">
             <label htmlFor="search">Search:</label>
             <input type={'text'} id="search" className="searchInput"></input>
-            <button className="searchButton" type="submit">Submit</button>
+            <button onClick={searchFilter} className="searchButton" type="submit">Submit</button>
         </form>
         <ul className="pList">
             {products.map((product: any, index: number) => (
                 <li className="pCard" key={index}>
                     <h3>{product['name']}</h3>
+                    <img src={product.img_link}></img>
                     {product.description} <br></br>
                     Price: ${product.price} <br></br>
                     Category: {product.category}
-
                 </li>
             ))}
         </ul>
